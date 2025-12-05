e7f26cfa9ba8d1e1c4540790643f231c148a4af5
raufurislam <raufurislam@gmail.com>
2025-12-05 13:16:32 +0600

    feat(auth): create register ui

--- src/app/(commonLayout)/(auth)/login/page.tsx
+++ src/app/(commonLayout)/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
-export default function page() {
+export default function LoginPage() {
   return (
     <div>
-      <h1>This is page component</h1>
+      <h1>This is LoginPage component</h1>
     </div>
   );
 }
---
+++ src/components/login-form.tsx
@@ -0,0 +1,7 @@
+export default function LoginForm() {
+  return (
+    <div>
+      <h1>This is login-form component</h1>
+    </div>
+  );
+}
