8099e5400dbc15db03419fed9497b972dc28d90c
raufurislam <raufurislam@gmail.com>
2026-01-03 21:45:40 +0600

    feat(proxy): setting up proxy file part-1

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,18 +1,57 @@
+import { jwtDecode } from "jwt-decode";
 import { NextResponse, NextRequest } from "next/server";
+
+interface userInterface {
+  id: string;
+  email: string;
+  role: "ADMIN" | "DOCTOR" | "PATIENT";
+  exp: number;
+  iat: number;
+}
+
+const roleBasedRoutes = {
+  ADMIN: [
+    "/admin/dashboard",
+    "/admin/manage-doctors",
+    "/admin/manage-patients",
+  ],
+  DOCTOR: ["/doctor/dashboard"],
+  PATIENT: [
+    "/patient/dashboard",
+    "/patient/appointments",
+    "/patient/medical-records",
+  ],
+};
 
 const authRoutes = ["/login", "/register", "/forgot-password"];
 
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
 
   const { pathname } = request.nextUrl;
 
-  if (!accessToken && !refreshToken) {
+  if (!accessToken && !refreshToken && !authRoutes.includes(pathname)) {
     return NextResponse.redirect(
       new URL(`/login?redirect=${pathname}`, request.url)
     );
   }
+
+  let user: userInterface | null = null;
+
+  if (accessToken) {
+    try {
+      user = jwtDecode(accessToken);
+      console.log({ proxyts: user });
+    } catch (err) {
+      console.log("Error decoding access token: ", err);
+      return NextResponse.redirect(
+        new URL(`/login?redirect=${pathname}`, request.url)
+      );
+    }
+  }
+
+  // if()
 
   return NextResponse.next();
 }
