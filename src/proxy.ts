40845f2a87149e0eb69c474ec483f97d3ae80beb
raufurislam <raufurislam@gmail.com>
2026-01-09 22:06:05 +0600

    feat(proxy): type definations & Routes configuration in proxy file

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,42 @@
+import { Route } from "next";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+type UserRole = "ADMIN" | "DOCTOR" | "PATIENT";
 
+// exact : ["/my-profile", "/settings"]
+// pattern : [/^\/dashboard/, /^\/patient/], // Routes stating with /dashboard or /patient
+type RouteConfig = {
+  exact: string[];
+  pattern: RegExp[];
+};
+
+const authRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "reset-password",
+];
+
+const commonProtectedRoutes: RouteConfig = {
+  exact: ["/my-profile", "settings"],
+  pattern: [], // [/password/change-password, /password/reset-password => /password/*]
+};
+
+const doctorProtectedRoutes: RouteConfig = {
+  pattern: [/^\/doctor/], // Routes starting with /doctor/* , /assistants, /appointments/*
+  exact: [], // assistants
+};
+
+const adminProtectedRoutes: RouteConfig = {
+  pattern: [/^\/admin/], // Routes starting with /admin/*
+  exact: [], // "/admins"
+};
+
+const patientProtectedRoutes: RouteConfig = {
+  pattern: [/^\/dashboard/], // Routes starting with /dashboard/*
+  exact: [], // "/medical-records"
+};
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
