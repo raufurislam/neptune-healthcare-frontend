9dd7e21f98feb291e8a7e6091df717b26b87bf73
raufurislam <raufurislam@gmail.com>
2026-01-10 20:26:14 +0600

    feat(proxy): protecting and authorizing the pages with main proxy function part-1.1

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,5 @@
-import { Route } from "next";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -83,8 +84,29 @@ const getDefaultDashboardRoute = (role: UserRole): string => {
 };
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  console.log("pathname", request.nextUrl.pathname);
+export async function proxy(request: NextRequest) {
+  const cookieStore = await cookies();
+  const pathname = request.nextUrl.pathname;
+
+  const accessToken = request.cookies.get("accessToken")?.value || null;
+
+  let userRole: UserRole | null = null;
+  if (accessToken) {
+    const verifiedToken: JwtPayload | string = jwt.verify(
+      accessToken,
+      process.env.JWT_SECRET as string
+    );
+
+    if (typeof verifiedToken === "string") {
+      cookieStore.delete("accessToken");
+      cookieStore.delete("refreshToken");
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    userRole = verifiedToken.role;
+  }
+
+  console.log(userRole);
 
   return NextResponse.next();
 }
